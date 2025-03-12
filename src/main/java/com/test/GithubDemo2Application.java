7a8357e0226d9d1fd75b893316e0f57c5c4157e9
Anita <anita02sahu@gmail.com>
2025-03-12 11:28:41 +0530

    defectfix-3456-login-error

--- src/main/java/com/test/GithubDemo2Application.java
+++ src/main/java/com/test/GithubDemo2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubDemo2Application {
 
+	int x=10;
 	public static void main(String[] args) {
 		SpringApplication.run(GithubDemo2Application.class, args);
 	}
