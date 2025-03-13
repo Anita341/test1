a1e012569799470702dcf68e0326121c20cff086
Bijay Kumar Sahu <ku.bijaybpd@gmail.com>
2025-03-13 17:26:43 +0530

    Update GithubDemo2Application.java

--- src/main/java/com/test/GithubDemo2Application.java
+++ src/main/java/com/test/GithubDemo2Application.java
@@ -8,6 +8,7 @@ public class GithubDemo2Application {
 
 	int x=10;
 	int y=10000;
+	String name = "bijay";
 	public static void main(String[] args) {
 		SpringApplication.run(GithubDemo2Application.class, args);
 	}
